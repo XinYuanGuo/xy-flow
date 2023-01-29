1c63f4c8b4cc6a580f2337e1018a3149bbd744ec
guoxinyuan <guoxy@flatincbr.com>
2023-01-29 23:50:15 +0800

    feat: publish packages

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 module.exports = core;
 
 function core() {
-    return "Hello from core";
+  return "Hello from core 111";
 }
--- packages/utils/lib/utils.js
+++ packages/utils/lib/utils.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 module.exports = utils;
 
 function utils() {
-    return "Hello from utils";
+  return "Hello from utils 111";
 }
