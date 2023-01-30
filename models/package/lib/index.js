9470d4d57b5cfae5a17f12745a6c145bd0795b01
guoxinyuan <guoxy@flatincbr.com>
2023-01-30 18:27:40 +0800

    fix: 修改lerna.json配置 && 添加包

--- models/package/lib/index.js
+++ models/package/lib/index.js
@@ -2,6 +2,7 @@ import { getNpmLatestVersion } from "@xy-flow/get-npm-info";
 import fse from "fs-extra";
 import path from "path";
 import { pathExistsSync } from "path-exists";
+import pkgDir from "pkg-dir";
 
 export default class Package {
   constructor(
@@ -41,7 +42,27 @@ export default class Package {
   }
   async update() {}
 
-  getRootFilePath() {}
+  getRootFilePath() {
+    function _getRootFile(targetPath) {
+      // 1. 获取package.json所在目录
+      const dir = pkgDir.sync(targetPath);
+      if (dir) {
+        // 2. 读取package.json
+        const pkgFile = require(path.resolve(dir, "package.json"));
+        // 3. 寻找main/lib
+        if (pkgFile && pkgFile.main) {
+          // 4. 路径的兼容(macOS/windows)
+          return formatPath(path.resolve(dir, pkgFile.main));
+        }
+      }
+      return null;
+    }
+    if (this.storeDir) {
+      return _getRootFile(this.cacheFilePath);
+    } else {
+      return _getRootFile(this.targetPath);
+    }
+  }
 
   getSpecificCacheFilePath(packageVersion) {
     return path.resolve(
